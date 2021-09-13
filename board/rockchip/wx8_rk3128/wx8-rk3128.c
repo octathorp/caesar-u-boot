54db01a7b0d9c3c51e2c1a1150aaf6beb86d1963
Martin Cerveny <m.cerveny@computer.org>
2021-09-13 21:18:30 +0200

    WIP: added "download key - select", enabled PM
    - download does not work for SDCARD (unsupported by rockchip), but enters this mode
    - enabled PM allows use display for "logo" images, uBOOT is ok, but problems rises
    in switch to kernel (seems to be problems with irq "vblank"), images must be stored
    in ANDROID resources or FIT, needs also additions to kerbel dts/dtb.

--- board/rockchip/wx8_rk3128/wx8-rk3128.c
+++ board/rockchip/wx8_rk3128/wx8-rk3128.c
@@ -6,11 +6,51 @@
 
 #include <common.h>
 #include <dm.h>
+#include <asm/gpio.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
-int board_early_init_r(void)
+int rockchip_dnl_key_pressed(void)
 {
+	struct gpio_desc gpio;
+	ofnode node;
+	int press = false;
 
+	node = ofnode_path("/config");
+	if (!ofnode_valid(node)) {
+		debug("%s: no /config node?\n", __FUNCTION__);
+		return false;
+	}
+
+	if (gpio_request_by_name_nodev(node, "download,gpio", 0, &gpio, GPIOD_IS_IN)) {
+		debug("%s: no download,gpio\n", __FUNCTION__);
+		return false;
+	}
+
+	if (dm_gpio_is_valid(&gpio)) {
+		press = !dm_gpio_get_value(&gpio);
+	}
+
+	dm_gpio_free(NULL, &gpio);
+	return press;
+}
+
+int rk_board_late_init(void)
+{
+	struct gpio_desc gpio;
+	ofnode node;
+
+	node = ofnode_path("/config");
+	if (!ofnode_valid(node)) {
+		debug("%s: no /config node?\n", __FUNCTION__);
+		return 0;
+	}
+
+	if (gpio_request_by_name_nodev(node, "pm-enable,gpio", 0, &gpio, GPIOD_IS_OUT | GPIOD_IS_OUT_ACTIVE)) {
+		debug("%s: no pm-enable,gpio\n", __FUNCTION__);
+		return 0;
+	}
+
+	dm_gpio_free(NULL, &gpio);
 	return 0;
 }
